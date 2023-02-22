5cdd7918d89468eb99c87643782141c05aa67745
Muxammadali <115696592+Mukhammaddaali@users.noreply.github.com>
2023-02-22 21:03:14 +0500

    done

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -2,5 +2,6 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
+        if (false) System.out.println("true"); else System.out.println("true");
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task1/PositiveNumberDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task1/PositiveNumberDeterminer.java
@@ -1,6 +1,15 @@
 package school.mjc.stage0.conditions.task1;
 
+import java.util.Scanner;
+
 public class PositiveNumberDeterminer {
     public void isPositive(int numberToBeDetermined) {
+        if(numberToBeDetermined>0) System.out.println("true");else System.out.println("false");
+    }
+    public void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int a;
+        a=sc.nextInt();
+        isPositive(a);
     }
 }
